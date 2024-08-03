33494adadf03fe380c5527c5d7e1f3fab03405fe
Moayyad Alajlouni <moaead-alajlouni@hotmail.com>
2024-08-03 16:32:38 +0300

    add CandidateContext

---
+++ JobCandidateHub/Configurations/CandidateContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using JobCandidateHub.Models;
+
+namespace JobCandidateHub.Configurations
+{
+    public class CandidateContext : DbContext
+    {
+        public CandidateContext(DbContextOptions<CandidateContext> options) : base(options) { }
+
+        public DbSet<Candidate> Candidates { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Candidate>()
+                .HasIndex(c => c.Email)
+                .IsUnique();
+        }
+    }
+}
