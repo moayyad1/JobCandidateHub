a9cd1f348c7c03a6a122aedd1b0824c84c801793
Moayyad Alajlouni <moaead-alajlouni@hotmail.com>
2024-08-03 16:31:53 +0300

    add controllers

---
+++ JobCandidateHub/Controllers/CandidatesController.cs
@@ -0,0 +1,30 @@
+﻿using JobCandidateHub.DTOs;
+using JobCandidateHub.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobCandidateHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidatesController : ControllerBase
+    {
+        private readonly ICandidateService _candidateService;
+
+        public CandidatesController(ICandidateService candidateService)
+        {
+            _candidateService = candidateService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddOrUpdateCandidate([FromBody] CandidateDto candidateDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _candidateService.AddOrUpdateCandidateAsync(candidateDto);
+            return Ok("Success");
+        }
+    }
+}
---
+++ JobCandidateHub/Controllers/HealthCheckController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobCandidateHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { Status = "Healthy" });
+        }
+    }
+}
