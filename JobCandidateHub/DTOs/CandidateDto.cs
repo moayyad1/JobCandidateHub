f0fee4a0285ca8ddd6a1bab224c2589eef121dea
Moayyad Alajlouni <moaead-alajlouni@hotmail.com>
2024-08-03 16:32:24 +0300

    add Candidate model and DTO

---
+++ JobCandidateHub/DTOs/CandidateDto.cs
@@ -0,0 +1,15 @@
+﻿namespace JobCandidateHub.DTOs
+{
+    public class CandidateDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string PreferredCallTime { get; set; }
+        public string LinkedInProfileUrl { get; set; }
+        public string GitHubProfileUrl { get; set; }
+        public string Comment { get; set; }
+    }
+
+}
---
+++ JobCandidateHub/Models/Candidate.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobCandidateHub.Models
+{
+    public class Candidate
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First name is required.")]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        [StringLength(50, ErrorMessage = "Last name cannot be longer than 50 characters.")]
+        public string LastName { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address format.")]
+        public string Email { get; set; }
+
+        public string? PreferredCallTime { get; set; }
+
+        public string? LinkedInProfileUrl { get; set; }
+
+        public string? GitHubProfileUrl { get; set; }
+
+        [Required(ErrorMessage = "Comment is required.")]
+        [StringLength(500, ErrorMessage = "Comment cannot be longer than 500 characters.")]
+        public string Comment { get; set; }
+    }
+}
